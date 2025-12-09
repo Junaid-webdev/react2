cc7eec81ab5a68e844378e56c7fdb8e07e0c3403
junaid <mohmmadjunaidqurashi@gmail.com>
2025-12-09 15:58:09 +0530

    JSON Server API and Loader

--- react-router-tut/src/App.jsx
+++ react-router-tut/src/App.jsx
@@ -2,37 +2,37 @@ import { useEffect, useState } from "react";
 
 export default function App() {
 
-  const [userData,setData]=useState([]);
-  useEffect(() => {
-    getUserData();
-  }, [])
+  const [userData,setUserData]=useState([]);
+  const [loding,setLoding]=useState(false)
+  useEffect(()=>{
+    setLoding(true)
+getUserData();
+  },[])
 
-  async function getUserData() {
-    const url = "https://jsonplaceholder.typicode.com/users";
-    let response = await fetch(url)
-    response = await response.json()
-  setData(response)
+  const getUserData = async ()=>{
+    const url = "http://localhost:3000/users";
+    let response = await fetch(url);
+    response = await response.json();
+    console.log(response);
+    setUserData(response)
+    setLoding(false)
+    
   }
-  console.log(userData);
-  
 
   return <>
-<h1>Fetch data form api </h1>
-<ul style={{ display:"flex",justifyContent:"space-around", border:"1px solid #aaa",margin:"0px",padding:"10px", fontSize:"20px",listStyle:"none",background:"#ccc"}}>
-      <li style={{ textAlign:"center" }}>FirstName</li>
-      <li style={{ textAlign:"center" }}>Last Name</li>
-      <li style={{ textAlign:"center" }}>Email</li>
-      <li style={{ textAlign:"center" }}>Address</li>
-     </ul>
-{
-    userData && userData.map((user,index)=>(
-     <ul key={index} style={{ display:"flex",justifyContent:"space-around", border:"1px solid #aaa",margin:"0px",padding:"10px", listStyle:"none",  }}>
-      <li style={{ textAlign:"center" }}>{user.name}</li>
-      <li style={{ textAlign:"center" }}>{user.username}</li>
-      <li style={{ textAlign:"center" }}>{user.email}</li>
-      <li style={{ textAlign:"center" }}>{user.address.street}</li>
-     </ul>
-  ))
-}
+<div>
+  <h1>Integrate JSON Server API</h1>
+  {
+    !loding?
+    userData.map((user,index)=>(
+   <ul key={index} style={{ display:"flex",justifyContent:"space-around", border:"1px solid #aaa",margin:"0px",padding:"10px", listStyle:"none",}}>
+    <li>{user.name}</li>
+    <li>{user.age}</li>
+    <li>{user.email}</li>
+   </ul>
+    ))
+    :<h1>Loding Data.......</h1>
+  }
+</div>
   </>
 }
