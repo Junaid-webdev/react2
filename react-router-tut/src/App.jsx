e5aa280cfa4ddf80484896e59ba6faaaf5b32cac
junaid <mohmmadjunaidqurashi@gmail.com>
2025-12-10 16:11:06 +0530

    | React New Update,

--- react-router-tut/src/App.jsx
+++ react-router-tut/src/App.jsx
@@ -1,45 +1,38 @@
-import { useActionState, useState } from "react";
+import { useActionState, useState,Activity } from "react";
 import "./App.css";
 function App() {
 
-
-
-  const handleLogin = (prevData, formData) => {
-    let name = formData.get('name')
-    let password = formData.get('password')
-    let regex = /^[A-Z0-9]+$/i;
-
-    if (!name || name.length > 8) {
-      return { error: "Only 8 charcters ",name,password }
-    } else if (!regex.test(password)) {
-      return { error: "Only numbers and alphabetes",name,password }
-    }
-    return { message: "You are successfully login!" }
-  }
-
-  const [data, formAction, pedding] = useActionState(handleLogin);
-  console.log(data);
+  const [showHome,setShowHome]=useState(true)
 
   return <>
     <div>
+  <h1>Activity in React 19.2</h1>
+     <button onClick={()=>setShowHome(true)}>Home</button>
+     <button onClick={()=>setShowHome(false)}>User Form</button>
+     <Activity mode={showHome==true?'visible':'hidden'}>
+      <Home/>
+     </Activity>
+     <Activity mode={showHome==false?'visible':'hidden'}>
+      <UserForm/>
+     </Activity>
+    </div>
+  </>
+}
+function Home(){
+  return(
+    <h1>Home Page</h1>
+  )
+}
 
-      <h1>Validation with UseAction in React  </h1>
-      {data?.message && <h2 style={{ color: "green" }}>{data.message}</h2>}
-      {data?.error && <h2 style={{ color: "red" }}>{data.error}</h2>}
-      <form action={formAction}>
-        <input defaultValue={data?.name} type="text" name="name" placeholder="Enter Your Name" />
-        <br /><br />
-        <input defaultValue={data?.password} type="text" name="password" placeholder="Enter user Password" />
-        <br /><br />
-        <button type="submit">Login</button>
-          </form>
+function UserForm(){
+  return(
+    <div>
+      <h2>User Form</h2>
+      <input type="text" placeholder="Enter Name" />
+      <input type="text" placeholder="Enter Password" />
+      <input type="text" placeholder="Enter email" />
 
     </div>
-
-
-
-  </>
-
-
+  )
 }
 export default App;
